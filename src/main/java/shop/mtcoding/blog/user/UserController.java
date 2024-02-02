ef1680391f51a38f1d5cb6b40420356ef41c4bd5
egdg4587 <egdg7777@gmail.com>
2024-02-02 14:46:08 +0900

    버전변경최신

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,12 +1,10 @@
 package shop.mtcoding.blog.user;
 
-import lombok.Data;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpSession;
 
 
 /**
